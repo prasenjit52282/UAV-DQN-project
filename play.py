8381d37c33fac05edc4cf2a715f798851238ca06
prasenjit52282 <prasenjitkarmakar52282@gmail.com>
2021-11-02 11:31:26 +0530

    latest pay file edit

--- play.py
+++ play.py
@@ -18,10 +18,10 @@ logger=TensorboardLogger(loc="./logs/",experiment="DQN")
 #%% Collecting
 print("Collecting random transitions .....")
 curr_state=env.reset()
-for _ in range(100):
+for _ in range(50000):
     num_acts=len(curr_state["uavs"])
-    acts=env.convert_action_arr([np.random.randint(0,125) for a in range(num_acts)])
-    next_state,reward,done,info=env.step(acts)
+    acts=[np.random.randint(0,125) for a in range(num_acts)]
+    next_state,reward,done,info=env.step(env.convert_action_arr(acts))
     agent.memory.push(curr_state, acts, reward, next_state, not done)
     if done:
         curr_state=env.reset()
@@ -54,8 +54,9 @@ for step in range(1,1000000+1):
     if step%10000==0:
         agent.updateFixedQ()
            
-    if step%4==0:pass
-        #agent.learn(batch_size=32)
+    if step%50==0:#pass
+        print("Learning from Replay-Buffer")
+        agent.learn(batch_size=32)
 
 #%% Testing   
 print("Testing ......")
